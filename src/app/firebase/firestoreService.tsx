339752dc41bbf1d618856f1c18168bbefa626c84
Anway Somani <anway@transvision.us>
2025-02-09 17:07:48 +0530

    Updated firestore to show only active categories

--- src/app/firebase/firestoreService.tsx
+++ src/app/firebase/firestoreService.tsx
@@ -1,12 +1,23 @@
 import { db } from "./firebaseConfig";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, getDoc, doc } from "firebase/firestore";
 
 const dataCollection = collection(db, "catalogue");
 
 export const getData = async () => {
     try {
         const snapshot = await getDocs(dataCollection);
-        return snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+        const activeCategories = [];
+
+        for (const categoryDoc of snapshot.docs) {
+            const categoryRef = doc(db, "catalogue", categoryDoc.id);
+            const categorySnapshot = await getDoc(categoryRef);
+
+            if (categorySnapshot.exists() && categorySnapshot.data().isActive) {
+                activeCategories.push({ id: categoryDoc.id, ...categoryDoc.data() });
+            }
+        }
+
+        return activeCategories;
     } catch (error) {
         console.error("Error fetching data:", error);
         return [];
